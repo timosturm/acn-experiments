2c2758d66e2c183116b1153681b081ddaa33c3ab
Timo Sturm <timo.sturm@posteo.de>
2024-11-06 12:53:46 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+"""
+Setup for gymportal.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="acn_experiments",
+    version="0.0.1",
+    description="Experiments for RL with the ACN Research Portal.",
+    packages=find_packages(),
+    package_data={
+        "acn_experiments": [
+            "pv/data/*",
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "gymportal @ git+https://git.ies.uni-kassel.de/mhassouna/acnsimulation_gym",
+        "ray[rllib]==2.10.0",
+        "gputil",
+        "jupyter",
+        "tqdm",
+        "grpcio==1.62.0",
+        "icecream",
+        "tensorflow==2.12.0",
+        "torch",
+    ],
+)
