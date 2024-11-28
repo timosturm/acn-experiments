ebebee14eefb8e5b9ec3fb17958c3bc57c270f78
Timo Sturm <timo.sturm@student.uni-kassel.de>
2024-11-28 11:20:36 +0100

    Removed observation wrapper because it is to complicated to use.

--- src/cleanRL/environment.py
+++ src/cleanRL/environment.py
@@ -12,9 +12,9 @@ def make_env(config, gamma, seed: int = None):
         env = gym.wrappers.FlattenObservation(env)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(
-            env, lambda obs: np.clip(obs, -10, 10))
+        # env = gym.wrappers.NormalizeObservation(env)
+        # env = gym.wrappers.TransformObservation(
+        #     env, lambda obs: np.clip(obs, -10, 10))
         env = gym.wrappers.NormalizeReward(env, gamma=gamma)
         env = gym.wrappers.TransformReward(
             env, lambda reward: np.clip(reward, -10, 10))
