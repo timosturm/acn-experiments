9ae3da4b06ef026ff4b6f91bc7fa6ca8c926e7b5
Timo Sturm <timo.sturm@student.uni-kassel.de>
2024-12-02 11:40:45 +0100

    Updated sh script.

--- cleanRL_search_transformer/run_search.sh
+++ cleanRL_search_transformer/run_search.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 for a in 5 10 20 50 80 100 150 200
 do
-    # for d in A B C D
-    # do
-        d=$1
+    for d in A B C D
+    do
         session="search_t=${a}_r=${d}"
         session="${session//"."/$"dot"}"
 
         tmux new-session -ds "$session" "python run_search.py --transformer-cap $a --reward-cfg $d"
         tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
-    # done
+    done
 done
