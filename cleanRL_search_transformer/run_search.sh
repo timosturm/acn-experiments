980db15b34663be74b0970f06760483d9a69d32c
Timo Sturm <timo.sturm@student.uni-kassel.de>
2024-12-09 15:41:36 +0100

    Updated start up script for real-world-runs

--- cleanRL_search_transformer/run_search.sh
+++ cleanRL_search_transformer/run_search.sh
@@ -1,12 +1,50 @@
 #!/bin/bash
-for a in 5 10 20 50 80 100 150 200
-do
-    for d in A B C D
-    do
-        session="search_t=${a}_r=${d}"
-        session="${session//"."/$"dot"}"
 
-        tmux new-session -ds "$session" "python run_search.py --transformer-cap $a --reward-cfg $d"
-        tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
+# ent_coef=0.0001
+# learning_rate=0.001
+# for transformer_cap in 10 30 150 300
+# do
+#     for reward_cfg in A B C D
+#     do
+#         session="search_t=${transformer_cap}_r=${reward_cfg}"
+#         session="${session//"."/$"dot"}"
+
+#         tmux new-session -ds "$session" "python run_search.py --transformer-cap $transformer_cap --reward-cfg $reward_cfg --ent-coef $ent_coef --learning-rate $learning_rate"
+#         tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
+
+#         # sbatch -J $session run.sh $transformer_cap $reward_cfg $ent_coef $learning_rate
+#     done
+# done
+
+for ent_coef in 0.001 0.0001 0.00001
+do
+    for learning_rate in 0.001 0.0001 0.00001 0.000001
+    do
+        for transformer_cap in 80 100 200
+        do
+            reward_cfg=D
+
+            session="search_t=${transformer_cap}_r=${reward_cfg}_ent_coef=${ent_coef}_learning_rate=${learning_rate}"
+            session="${session//"."/$"dot"}"
+            sbatch -J $session run.sh $transformer_cap $reward_cfg $ent_coef $learning_rate
+        done
+
+        for transformer_cap in 5 10 20 80
+        do
+            reward_cfg=C
+
+            session="search_t=${transformer_cap}_r=${reward_cfg}_ent_coef=${ent_coef}_learning_rate=${learning_rate}"
+            session="${session//"."/$"dot"}"
+            sbatch -J $session run.sh $transformer_cap $reward_cfg $ent_coef $learning_rate
+        done
+
+        for transformer_cap in 5 20
+        do
+            reward_cfg=B
+
+            session="search_t=${transformer_cap}_r=${reward_cfg}_ent_coef=${ent_coef}_learning_rate=${learning_rate}"
+            session="${session//"."/$"dot"}"
+            sbatch -J $session run.sh $transformer_cap $reward_cfg $ent_coef $learning_rate
+        done
     done
 done
