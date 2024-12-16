9ae7ee527eb14134fa317626d0daa658815eaa23
Timo Sturm <timo.sturm@student.uni-kassel.de>
2024-12-16 11:10:38 +0100

    Updated run scripts.

--- cleanRL_search_transformer/run_one.sh
+++ cleanRL_search_transformer/run_one.sh
@@ -1,8 +1,14 @@
 a=50
 d=A
+e=1
 
-session="search_t=${a}_r=${d}"
+ent_coef=0.0001
+learning_rate=0.0003
+
+session="search_t=${a}_r=${d}_e=${e}"
 session="${session//"."/$"dot"}"
 
-tmux new-session -ds "$session" "python run_search.py --transformer-cap $a --reward-cfg $d"
-tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
+# tmux new-session -ds "$session" "python run_search.py --transformer-cap $a --reward-cfg $d"
+# tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
+
+sbatch -J $session run.sh $a $d $ent_coef $learning_rate $e
