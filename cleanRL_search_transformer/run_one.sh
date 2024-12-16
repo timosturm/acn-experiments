c119884a7af06623c05ba96b5df868d0705d49f8
Timo Sturm <timo.sturm@student.uni-kassel.de>
2024-12-16 11:20:56 +0100

    CHanged transformer cap and reward.

--- cleanRL_search_transformer/run_one.sh
+++ cleanRL_search_transformer/run_one.sh
@@ -1,5 +1,5 @@
-a=50
-d=A
+a=100
+d=C
 e=1
 
 ent_coef=0.0001
@@ -8,7 +8,7 @@ learning_rate=0.0003
 session="search_t=${a}_r=${d}_e=${e}"
 session="${session//"."/$"dot"}"
 
-# tmux new-session -ds "$session" "python run_search.py --transformer-cap $a --reward-cfg $d"
-# tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
+tmux new-session -ds "$session" "python run_search.py --transformer-cap $a --reward-cfg $d"
+tmux pipe-pane -ot ${session} "cat > $PWD/${session}.ansi"
 
-sbatch -J $session run.sh $a $d $ent_coef $learning_rate $e
+# sbatch -J $session run.sh $a $d $ent_coef $learning_rate $e
