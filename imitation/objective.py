2f4c023d3b35e7156f83cc05f32c1f619bbc9c21
Timo Sturm <timo.sturm@student.uni-kassel.de>
2025-02-26 11:16:16 +0100

    Refactoring of experiment files.

---
+++ imitation/objective.py
@@ -0,0 +1,147 @@
+import optuna
+import wandb
+from uuid import uuid4
+from torch.utils.tensorboard import SummaryWriter
+
+
+from copy import deepcopy
+
+from optuna import TrialPruned
+
+import random
+
+import gymnasium as gym
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from icecream import ic
+from src.imitation.args import MyArgs
+from imitation import imitate
+from rl import train_ppo
+from utils import save_state_dict, validate_on_env, clean_up
+
+
+def objective(
+    trial: optuna.trial.Trial,
+    args: MyArgs,
+    make_env,  # TODO Typing
+) -> float:
+    args = deepcopy(args)  # because arguments are copy-by-reference
+
+    args.imitation.lr = trial.suggest_float(
+        "lr_imitation", 1e-5, 1e-2, log=True)
+    args.imitation.n_epochs = trial.suggest_categorical(
+        "n_epochs_imitation", [5, 10, 20, 30, 40])
+
+    args.rl.lr = trial.suggest_float(
+        "lr_rl", 3e-5, 3e-3, log=True)
+    args.rl.ent_coef = trial.suggest_float("ent_coef_rl", 1e-6, 1e-2, log=True)
+    args.rl.gamma = trial.suggest_float("gamma_rl", 0.9, 0.99)
+    args.rl.gae_lambda = trial.suggest_float("gae_lambda_rl", 0, 1)
+    args.rl.max_grad_norm = trial.suggest_float("max_grad_norm_rl", 0.3, 0.7)
+    args.rl.vf_coef = trial.suggest_float("vf_coef_rl", 0, 1)
+    args.rl.clip_coef = trial.suggest_float("clip_coef_rl", 0, 1)
+    args.rl.num_steps = trial.suggest_float(
+        "num_steps / batch_size", 128, 2048, log=True)
+
+    args.rl.batch_size = int(args.rl.num_envs * args.rl.num_steps)
+    args.rl.minibatch_size = int(args.rl.batch_size // args.rl.num_minibatches)
+    args.rl.num_iterations = args.rl.total_timesteps // args.rl.batch_size
+
+    args.rl.envs = gym.vector.SyncVectorEnv(
+        [make_env(args.rl.config, args.rl.gamma, i)
+         for i in range(args.rl.num_envs)]
+    )
+
+    args.wandb_tags = [f"{key}={value}" for key, value in trial.params.items()]
+
+    run_name = f"{args.exp_name}__{uuid4()}"
+
+    wandb.init(
+        project=args.wandb_project_name,
+        entity=args.wandb_entity,
+        sync_tensorboard=True,
+        config=trial.params,  # vars(args),
+        name=run_name,
+        save_code=True,
+        group=args.wandb_group,
+        tags=args.wandb_tags,
+    )
+
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % (
+            "\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available()
+                          and args.cuda else "cpu")
+
+    # Imitatation Learning
+    best_state_dict = None
+    old_return = -np.inf
+    best_return = -np.inf
+
+    metadata = {
+        "parameter": trial.params
+    }
+
+    for epoch, state_dict in imitate(args.imitation, writer, device, state_dict=None):
+
+        eval_sim, new_return = validate_on_env(args.eval, state_dict)
+        writer.add_scalar("imitation/return", new_return, epoch)
+
+        for metric_name, f in args.eval.metrics.items():
+            writer.add_scalar(
+                f"imitation/{metric_name}", f(eval_sim), epoch)
+
+        metadata |= {"imitation": {"return": new_return, "epoch": epoch}}
+        save_state_dict(args, run_name, state_dict, "imitation",
+                        epoch, metadata=metadata)
+
+        if new_return > old_return:
+            best_return = new_return
+            best_state_dict = state_dict
+            save_state_dict(args, run_name, state_dict, "imitation",
+                            i="best", metadata=metadata)
+
+        old_return = new_return
+
+    assert best_state_dict, "Something went wrong, there is no best_state_dict!"
+
+    old_return = 0
+    # Reinforcement Learning
+    for i, (global_step, state_dict) in enumerate(train_ppo(args.rl, writer, device, state_dict=best_state_dict)):
+        eval_sim, new_return = validate_on_env(args.eval, state_dict)
+        writer.add_scalar("eval/return", new_return, global_step)
+
+        for metric_name, f in args.eval.metrics.items():
+            writer.add_scalar(f"eval/{metric_name}", f(eval_sim), global_step)
+
+        metadata |= {"rl": {"return": new_return, "global_step": global_step}}
+        save_state_dict(args, run_name, state_dict, "rl",
+                        global_step, metadata=metadata)
+
+        if new_return > old_return:
+            best_return = new_return
+            best_state_dict = state_dict
+            save_state_dict(args, run_name, state_dict, "rl",
+                            i="best", metadata=metadata)
+
+        trial.report(new_return, i)
+        if trial.should_prune():
+            clean_up(args, writer)
+            raise TrialPruned()
+
+        old_return = new_return
+
+    clean_up(args, writer, wandb.run)
+
+    return best_return
---
+++ imitation/rl.py
@@ -0,0 +1,215 @@
+from torch.utils.tensorboard import SummaryWriter
+
+
+from typing import Generator, Optional, OrderedDict, Tuple
+
+from src.cleanRL.agent import Agent
+import time
+
+import gymnasium as gym
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from icecream import ic
+from src.imitation.args import RLArgs
+
+
+def train_ppo(
+    args: RLArgs,
+    writer: SummaryWriter,
+    device: str,
+    state_dict: Optional[OrderedDict],
+) -> Generator[Tuple[int, OrderedDict], None, None]:
+    envs = args.envs
+
+    assert isinstance(envs.single_action_space,
+                      gym.spaces.Box), "only continuous action space is supported"
+
+    agent = Agent(
+        observation_shape=np.array(envs.single_observation_space.shape).prod(),
+        action_shape=np.array(envs.single_action_space.shape).prod()
+    ).to(device)
+
+    if state_dict:
+        agent.load_state_dict(state_dict)
+
+    optimizer = optim.Adam(agent.parameters(), lr=args.lr)
+
+    # ALGO Logic: Storage setup
+    obs = torch.zeros((args.num_steps, args.num_envs) +
+                      envs.single_observation_space.shape).to(device)
+    actions = torch.zeros((args.num_steps, args.num_envs) +
+                          envs.single_action_space.shape).to(device)
+    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+
+    # TRY NOT TO MODIFY: start the game
+    global_step = 0
+    start_time = time.time()
+    next_obs, _ = envs.reset()  # envs.reset(seed=args.seed)
+    next_obs = torch.Tensor(next_obs).to(device)
+    next_done = torch.zeros(args.num_envs).to(device)
+
+    for iteration in range(1, args.num_iterations + 1):
+        # Annealing the rate if instructed to do so.
+        if args.anneal_lr:
+            frac = 1.0 - (iteration - 1.0) / args.num_iterations
+            lrnow = frac * args.lr
+            optimizer.param_groups[0]["lr"] = lrnow
+
+        # collect trajectories
+        for step in range(0, args.num_steps):
+            global_step += args.num_envs
+            obs[step] = next_obs
+            dones[step] = next_done
+
+            # ALGO LOGIC: action logic
+            with torch.no_grad():
+                action, logprob, _, value = agent.get_action_and_value(
+                    next_obs)
+                values[step] = value.flatten()
+            actions[step] = action
+            logprobs[step] = logprob
+
+            # TRY NOT TO MODIFY: execute the game and log data.
+            next_obs, reward, terminations, truncations, infos = envs.step(
+                action.cpu().numpy())
+            next_done = np.logical_or(terminations, truncations)
+            rewards[step] = torch.tensor(reward).to(device).view(-1)
+            next_obs, next_done = torch.Tensor(next_obs).to(
+                device), torch.Tensor(next_done).to(device)
+
+            if "final_info" in infos:
+                for info in infos["final_info"]:
+                    if info and "episode" in info:
+                        print(
+                            f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                        writer.add_scalar("charts/episodic_return",
+                                          info["episode"]["r"], global_step)
+                        writer.add_scalar("charts/episodic_length",
+                                          info["episode"]["l"], global_step)
+
+            # log metrics for training
+            if torch.any(next_done):
+                sims = []
+                for info in infos["final_info"]:
+                    if info and "acn_interface" in info:
+                        sims.append(info["acn_interface"]._simulator)
+
+                for metric_name, f in args.metrics.items():
+                    value = np.mean([f(sim) for sim in sims])
+                    writer.add_scalar(
+                        f"train/{metric_name}", value, global_step)
+
+        # bootstrap value if not done
+        with torch.no_grad():
+            next_value = agent.get_value(next_obs).reshape(1, -1)
+            advantages = torch.zeros_like(rewards).to(device)
+            lastgaelam = 0
+            for t in reversed(range(args.num_steps)):
+                if t == args.num_steps - 1:
+                    nextnonterminal = 1.0 - next_done
+                    nextvalues = next_value
+                else:
+                    nextnonterminal = 1.0 - dones[t + 1]
+                    nextvalues = values[t + 1]
+                delta = rewards[t] + args.gamma * \
+                    nextvalues * nextnonterminal - values[t]
+                advantages[t] = lastgaelam = delta + args.gamma * \
+                    args.gae_lambda * nextnonterminal * lastgaelam
+            returns = advantages + values
+
+        # flatten the batch
+        b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+        b_logprobs = logprobs.reshape(-1)
+        b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+        b_advantages = advantages.reshape(-1)
+        b_returns = returns.reshape(-1)
+        b_values = values.reshape(-1)
+
+        # Optimizing the policy and value network
+        b_inds = np.arange(args.batch_size)
+        clipfracs = []
+        for epoch in range(args.update_epochs):
+            np.random.shuffle(b_inds)
+            for start in range(0, args.batch_size, args.minibatch_size):
+                end = start + args.minibatch_size
+                mb_inds = b_inds[start:end]
+
+                _, newlogprob, entropy, newvalue = agent.get_action_and_value(
+                    b_obs[mb_inds], b_actions[mb_inds])
+                logratio = newlogprob - b_logprobs[mb_inds]
+                ratio = logratio.exp()
+
+                with torch.no_grad():
+                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                    old_approx_kl = (-logratio).mean()
+                    approx_kl = ((ratio - 1) - logratio).mean()
+                    clipfracs += [((ratio - 1.0).abs() >
+                                   args.clip_coef).float().mean().item()]
+
+                mb_advantages = b_advantages[mb_inds]
+                if args.norm_adv:
+                    mb_advantages = (mb_advantages - mb_advantages.mean()
+                                     ) / (mb_advantages.std() + 1e-8)
+
+                # Policy loss
+                pg_loss1 = -mb_advantages * ratio
+                pg_loss2 = -mb_advantages * \
+                    torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+                pg_loss = torch.max(pg_loss1, pg_loss2).mean()
+
+                # Value loss
+                newvalue = newvalue.view(-1)
+                if args.clip_vloss:
+                    v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
+                    v_clipped = b_values[mb_inds] + torch.clamp(
+                        newvalue - b_values[mb_inds],
+                        -args.clip_coef,
+                        args.clip_coef,
+                    )
+                    v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
+                    v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
+                    v_loss = 0.5 * v_loss_max.mean()
+                else:
+                    v_loss = 0.5 * \
+                        ((newvalue - b_returns[mb_inds]) ** 2).mean()
+
+                entropy_loss = entropy.mean()
+                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
+
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(
+                    agent.parameters(), args.max_grad_norm)
+                optimizer.step()
+
+            if args.target_kl is not None and approx_kl > args.target_kl:
+                break
+
+        y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
+        var_y = np.var(y_true)
+        explained_var = np.nan if var_y == 0 else 1 - \
+            np.var(y_true - y_pred) / var_y
+
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        writer.add_scalar("charts/learning_rate",
+                          optimizer.param_groups[0]["lr"], global_step)
+        writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
+        writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
+        writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
+        writer.add_scalar("losses/old_approx_kl",
+                          old_approx_kl.item(), global_step)
+        writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
+        writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
+        writer.add_scalar("losses/explained_variance",
+                          explained_var, global_step)
+        print("SPS:", int(global_step / (time.time() - start_time)))
+        writer.add_scalar("charts/SPS", int(global_step /
+                                            (time.time() - start_time)), global_step)
+
+        yield global_step, agent.state_dict()
