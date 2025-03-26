f5d8a00557b96a5ce82e5f607300aec40d7effc5
Timo Sturm <timo.sturm@student.uni-kassel.de>
2025-03-26 18:37:05 +0100

    Added missing doc string.

--- src/pv/aux.py
+++ src/pv/aux.py
@@ -12,6 +12,15 @@ def _save_divide(a: np.ndarray, b: np.ndarray) -> np.ndarray:
 
 
 def pv_utilization(energy_total, pv_total) -> float:
+    """Calculates how much of the total charging energy (energy_total) was provided by PV.
+
+    Args:
+        energy_total (np.ndarray): Sum of charging rates for each timestep
+        pv_total (np.ndarray): Sum of PV energy for each timestep
+
+    Returns:
+        float:
+    """
     utilization = np.clip(energy_total, a_min=None, a_max=pv_total)
     utilization_ratio = _save_divide(utilization, energy_total)
 
@@ -19,6 +28,15 @@ def pv_utilization(energy_total, pv_total) -> float:
 
 
 def grid_use(energy_total, pv_total) -> float:
+    """Calculates how much of the total charging energy (energy_total) was provided by the grid (and not by PV).
+
+    Args:
+        energy_total (np.ndarray): Sum of charging rates for each timestep
+        pv_total (np.ndarray): Sum of PV energy for each timestep
+
+    Returns:
+        float:
+    """
     energy_grid = np.clip(energy_total - pv_total, a_min=0, a_max=None)
     ratio = _save_divide(energy_grid, energy_total)
 
@@ -26,8 +44,16 @@ def grid_use(energy_total, pv_total) -> float:
 
 
 def unused_pv(energy_total, pv_total) -> float:
-    pv_unused = np.clip(pv_total - energy_total, a_min=0, a_max=None)
+    """Calculates how much of the energy provided by PV (pv_total) was not used for charging.
+    
+    Args:
+        energy_total (np.ndarray): Sum of charging rates for each timestep
+        pv_total (np.ndarray): Sum of PV energy for each timestep
 
+    Returns:
+        float:
+    """
+    pv_unused = np.clip(pv_total - energy_total, a_min=0, a_max=None)
     ratio = _save_divide(pv_unused, pv_total)
 
     return np.mean(ratio)
