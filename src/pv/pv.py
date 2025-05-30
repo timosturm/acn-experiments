3a373227a3afa24c72ee5e8652e63f25ed9bd71e
Timo Sturm <timo.sturm@student.uni-kassel.de>
2025-05-30 10:48:58 +0200

    Catch numpy warnings when useing datetime with timezone.

--- src/pv/pv.py
+++ src/pv/pv.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from typing import List
+import warnings
 import numpy as np
 import pandas as pd
 from icecream import ic
@@ -28,7 +29,11 @@ def read_pv_data(csv: str) -> pd.DataFrame:
 def get_most_recent_P(df: pd.DataFrame, timesteps_as_dt: List[datetime]) -> List[float]:
     time_array = df['time'].values
     P_array = df['P'].values
-    timesteps_as_dt = np.array(timesteps_as_dt, dtype='datetime64[ns]')
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore", category=UserWarning)
+        timesteps_as_dt = np.array(timesteps_as_dt, dtype='datetime64[ns]')
 
     indices = np.searchsorted(time_array, timesteps_as_dt, side='right') - 1
 
