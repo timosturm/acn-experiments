f6199ab95a77c1f74f1b78ddd29dbe3bd798277f
Timo Sturm <timo.sturm@posteo.de>
2025-07-03 11:23:27 +0200

    Added a reward function.

--- src/pv/rewards.py
+++ src/pv/rewards.py
@@ -1,3 +1,4 @@
+import acnportal
 from gymportal.sim.simulators_custom import EvaluationSimulator
 from datetime import timedelta
 from typing import List, Callable, Any, Union
@@ -11,12 +12,15 @@ from gymportal.environment.interfaces import BaseSimInterface
 import pandas as pd
 from icecream import ic
 
+from src.pv.pv import get_most_recent_P
 from src.pv.utils import W_to_A
 
 from gymportal.environment import *
 import numpy as np
 from src.pv.utils import get_pvs_in_W, W_to_A
 import pandas as pd
+
+from src.rewards import f
 from .aux import grid_use, pv_utilization, _save_divide, unused_pv
 
 
@@ -107,6 +111,53 @@ def unused_pv_reward(df_pv: pd.DataFrame) -> SimReward:
 
     return SimReward(single_reward_function=single_reward,
                      multi_reward_function=multi_reward, name="unused_pv_penalty")
+
+
+def sparse_unused_pv_reward(df_pv: pd.DataFrame) -> SimReward:
+    def single_reward(env: BaseSimInterface) -> float:
+        evs_departing: List[acnportal.acnsim.EV] = [
+            ev for ev in env.interface._simulator.ev_history.values() if ev.departure == env.timestep
+        ]
+
+        if len(evs_departing) == 0:
+            return 0
+
+        arrivals = [ev.arrival for ev in evs_departing]
+        departure = env.timestep
+
+        def get_pvs(a, b) -> np.ndarray:
+            timesteps_as_dt = [
+                env.interface.timestep_to_datetime(t) for t in range(a, b, env.interface._simulator.period)
+            ]
+
+            pvs_in_W = get_most_recent_P(df_pv, timesteps_as_dt)
+            pvs_in_A = np.array(
+                [
+                    W_to_A(x, env.interface._simulator.network._voltages)
+                    for x in pvs_in_W
+                ]
+            )
+
+            return pvs_in_A
+
+        pvs = [get_pvs(arrival, departure) for arrival in arrivals]
+
+        def get_rates(a, b) -> np.ndarray:
+            return np.sum(env.interface.charging_rates[:, a:b], axis=0)
+
+        rates = [get_rates(arrival, departure) for arrival in arrivals]
+
+        s = 0
+        for p, r in zip(pvs, rates):
+            v = _save_divide(p - r, p)
+            s += np.mean(1 - np.clip(v, 0, None))
+
+        s /= len(arrivals)
+        return s
+
+    return SimReward(single_reward_function=single_reward,
+                     multi_reward_function=None,
+                     name="sparse_unused_pv_reward")
 
 
 def charging_reward() -> SimReward:
